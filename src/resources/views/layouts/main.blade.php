a3cf829a650146e105d2e63c39aa611b4dcf0896
Your Name <you@ShefiaAnggraeni37.gmail.com>
2023-05-10 11:03:52 +0700

    pert4_peweb

--- src/resources/views/layouts/main.blade.php
+++ src/resources/views/layouts/main.blade.php
@@ -2,17 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{config('app.name')}}</title>
+    <!-- <title>{{config('app.name')}}</title> -->
+    <title>@yield('title', $title)</title>
+    
     @include('partials.styles')
 </head>
 <body>
     @include('partials.header')
+    <h1>@yield('title', $title)</h1>
     <main class="container mt-5">
         @yield('content')
-    </main>  
+    </main>
 </body>
-@include('partials.footer')
-@include('partials.scripts')
+    
+    @include('partials.footer')
+    @include('partials.scripts')
 </html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home']);
 })->name('home');
 
-Route::get('/profile', function () {
-    return view('profile');
-})->name('profile');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
+Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::get('login', [UserController::class, 'login'])->name('login');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
+Route::get('password', [UserController::class, 'password'])->name('password');
+Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+Route::get('logout', [UserController::class, 'logout'])->name('logout');
